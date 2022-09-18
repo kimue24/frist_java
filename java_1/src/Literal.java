101d817aeab01ad29c8a965089a95244a1b39d9d
김민호 <54491294+kimue24@users.noreply.github.com>
2022-09-18 09:59:26 +0900

    20220918

---
+++ java_1/src/Literal.java
@@ -0,0 +1,30 @@
+public class Literal {
+    public static void main(String[] args) {
+        int var1 = 10;
+        System.out.println(var1);
+
+        int var2 = 010;
+        System.out.println(var2);
+
+        int var3 = 0x10;
+        System.out.println(var3);
+
+        double var4 = 0.25;
+        System.out.println(var4);
+
+        double var5 = 2E5;
+        System.out.println(var5);
+
+        char var6 = 'A';
+        System.out.println(var6);
+
+        char var7 = '한';
+        System.out.println(var7);
+
+        String var9 = "자바";
+        boolean var10 = true;
+        boolean var11 = false;
+        System.out.println(var10);
+        System.out.println(var11);
+    }
+}
---
+++ java_1/src/variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args) {
+        int value = 10;
+        int result = value + 10;
+        System.out.println(value);
+        System.out.println(result);
+    }
+}
