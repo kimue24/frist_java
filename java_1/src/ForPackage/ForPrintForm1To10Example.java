4fca304100659a20adbabe26b29b679225f3621b
김민호 <54491294+kimue24@users.noreply.github.com>
2022-10-09 13:25:14 +0900

    update

--- java_1/src/ForPackage/ForPrintForm1To10Example.java
+++
@@ -1,9 +0,0 @@
-package ForPackage;
-
-public class ForPrintForm1To10Example {
-    public static void main(String[] args) {
-        for(int i =1; i<=10; i++) {
-            System.out.println(i);
-        }
-    }
-}
